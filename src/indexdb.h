66afc2a2b015c40b1502dbce6794b49d1552c596
theluckyone <theluckyone@users.sourceforge.net>
2011-08-23 13:48:00 +0000

    Add some documentation to IndexDB class

--- src/indexdb.h
+++ src/indexdb.h
@@ -21,10 +21,33 @@ class IndexDB
 public:
     IndexDB(LibraryInfo *info);
     ~IndexDB();
+
+    /**
+      Open the index database
+      @throw BookException
+      */
     void open();
+
+    /**
+      Get the books list model that contians categories and books
+      */
     BooksListModel *booksListModel();
+
+    /**
+      Get categories list
+      */
     BooksListModel *catsListModel();
+
+    /**
+      Load the books list model asynchronously
+      */
     void loadBooksListModel();
+
+    /**
+      Get information about the specified Book
+      @param bookID the book id
+      @return BookInfo that contains information about the specified Book
+      */
     BookInfo *getBookInfo(int bookID);
     BookInfo *getQuranBook();
     LibraryInfo *connectionInfo();
@@ -35,13 +58,55 @@ public:
     void setConnectionInfo(LibraryInfo *info);
     void getShoroohPages(BookInfo *info);
 
+    /**
+      Change categorie title
+      @param catID categorie id
+      @param title the new title
+      */
     void updateCatTitle(int catID, QString title);
+
+    /**
+      Change a categorie parent
+      @param catID categorie id
+      @param parentID the new parent id
+      */
     void updateCatParent(int catID, int parentID);
+
+    /**
+      Chane categorie order in the books list model
+      @param catID categorie id
+      @param catOrder Categorie order
+      */
     void updateCatOrder(int catID, int catOrder);
+
+    /**
+      Create a new categorie
+      @param title Title of the new categorie
+      @return the new categorie id
+      */
     int addNewCat(const QString &title);
+
+    /**
+      Remove a categorie
+      @param catID categorie id
+      */
     void removeCat(int catID);
+
     void makeCatPlace(int parentID, int catOrder);
+
+    /**
+      Get categorie books count
+      @param catID categorie id
+      @return books count
+      */
     int booksCount(int catID);
+
+    /**
+      Change books categorie
+      @param fromCat The source categorie
+      @param toCat The target categorie
+      @return true on success
+      */
     bool moveCatBooks(int fromCat, int toCat);
 
 protected:
