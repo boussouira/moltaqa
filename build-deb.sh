cffb4d9168d3381b0e6c2a9d431c5e589d6c99b6
Ahmed Saidi <justroftest@gmail.com>
2011-11-05 19:48:16 +0000

    Edit ubuntu deb builder

--- build-deb.sh
+++ build-deb.sh
@@ -3,10 +3,13 @@
 APP_NAME="moltaqa-lib"
 APP_VERSION="0.8"
 APP_DIR_NAME="${APP_NAME}-${APP_VERSION}"
-APP_FILES="moltaqa-lib.pro CMakeLists.txt  Doxyfile  README  share  src  test"
+APP_FILES="moltaqa-lib.pro  Doxyfile share  src  test"
 TEMP_DIR="temp-build-deb"
 SOURCE_ARCHIVE="${APP_NAME}-${APP_VERSION}.tar.gz"
 SOURCE_ARCHIVE_ORIG="${APP_NAME}_${APP_VERSION}.orig.tar.gz"
+
+echo "[*] Remove temp dir: $TEMP_DIR"
+rm -rvf $TEMP_DIR >> /dev/null
 
 echo "[*] Create temp dir: $TEMP_DIR"
 mkdir $TEMP_DIR
@@ -21,12 +24,15 @@ echo "[*] Create application directory: $APP_DIR_NAME"
 mkdir $TEMP_DIR/$APP_DIR_NAME
 
 echo "[*] Copy files to application dir"
-cp -fvr $APP_FILES debian $TEMP_DIR/$APP_DIR_NAME  >> /dev/null
+cp -fvr $APP_FILES debian $TEMP_DIR/$APP_DIR_NAME >> /dev/null
 
 echo "[*] Enter application directory"
 cd $TEMP_DIR/$APP_DIR_NAME
 
 echo "[*] Start building deb"
 debuild >> /dev/null
+
+cd ..
+echo "[*] Generated deb:" *.deb
 
 echo "[*] Done"
