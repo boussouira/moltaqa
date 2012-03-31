046f4d8a29a001c708e738e4198af04595f51ca6
Ahmed Saidi <justroftest@gmail.com>
2012-03-31 22:40:27 +0000

    Fix clone page

--- src/bookpage.cpp
+++ src/bookpage.cpp
@@ -14,19 +14,5 @@ BookPage::BookPage()
 
 BookPage *BookPage::clone()
 {
-    BookPage *p = new BookPage();
-
-    p->sora = sora;
-    p->aya = aya;
-    p->ayatCount = ayatCount;
-    p->page = page;
-    p->pageID = pageID;
-    p->part = part;
-    p->haddit = haddit;
-    p->titleID = titleID;
-
-    p->text = text;
-    p->title = title;
-
-    return p;
+    return new BookPage(*this);
 }
