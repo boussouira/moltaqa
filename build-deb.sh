f214441a2e4c7ef3218d263b28562aff4d891749
Ahmed Saidi <justroftest@gmail.com>
2012-07-11 13:55:27 +0000

    Move generated file to a new dist folder

--- build-deb.sh
+++ build-deb.sh
@@ -6,9 +6,11 @@ APP_DIR_NAME="${APP_NAME}-${APP_VERSION}"
 APP_FILES="moltaqa-lib.pro  Doxyfile share bin  src  test"
 TEMP_DIR="temp-build-deb"
 RPM_TEMP_DIR="rpm-temp-dir"
-OUTPUT_DIR="output"
+OUTPUT_DIR="dist"
 SOURCE_ARCHIVE="${APP_NAME}-${APP_VERSION}.tar.gz"
 SOURCE_ARCHIVE_ORIG="${APP_NAME}_${APP_VERSION}.orig.tar.gz"
+SOURCE_DIR=`pwd`
+TEMP_DIR_PATH=$SOURCE_DIR/$TEMP_DIR
 
 echo "********************************"
 echo " Package: $APP_NAME"
@@ -22,8 +24,6 @@ echo "[*] Create temp directories"
 mkdir $TEMP_DIR
 mkdir -p $TEMP_DIR/$OUTPUT_DIR
 mkdir -p $TEMP_DIR/$RPM_TEMP_DIR
-
-TEMP_DIR_PATH=`pwd`/$TEMP_DIR
 
 echo "[*] Create source archive: $SOURCE_ARCHIVE"
 tar -zcf $TEMP_DIR/$SOURCE_ARCHIVE $APP_FILES
@@ -70,8 +70,14 @@ echo "[*] Create md5 sums"
 cd $TEMP_DIR_PATH/$OUTPUT_DIR
 md5sum -b * > MD5SUMS
 
-echo "[*] Generated files in $TEMP_DIR_PATH/$OUTPUT_DIR"
-cat $TEMP_DIR_PATH/$OUTPUT_DIR/MD5SUMS
+mv -v $TEMP_DIR_PATH/$OUTPUT_DIR $SOURCE_DIR
+
+echo "[*] Generated files in $SOURCE_DIR/$OUTPUT_DIR"
+cat $SOURCE_DIR/$OUTPUT_DIR/MD5SUMS
+
+echo "[*] Remove build directory"
+cd $SOURCE_DIR
+rm -fr $TEMP_DIR
 
 echo "[*] Done"
 
