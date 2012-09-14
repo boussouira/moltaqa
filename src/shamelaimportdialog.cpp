61e3ce02f94bd053d7dd6d9850ee1268fe47f8ac
Ahmed Saidi <justroftest@gmail.com>
2012-09-14 18:27:08 +0000

    Make a string translatable

--- src/shamelaimportdialog.cpp
+++ src/shamelaimportdialog.cpp
@@ -218,7 +218,7 @@ void ShamelaImportDialog::nextStep()
         setupImporting();
         startImporting();
         ui->pushNext->setEnabled(false);
-        ui->pushCancel->setText("إيقاف");
+        ui->pushCancel->setText(tr("إيقاف"));
         goPage();
     }
 }
