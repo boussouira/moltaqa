f24163ff9e1ee2229621ddcfa6cdcfce9d832622
Ahmed Saidi <justroftest@gmail.com>
2013-02-18 10:31:24 +0000

    Change a filter line icon

--- src/filterlineedit.cpp
+++ src/filterlineedit.cpp
@@ -18,7 +18,7 @@ FilterLineEdit::FilterLineEdit(QWidget *parent) :
     QIcon clearIcon = QIcon::fromTheme(layoutDirection() == Qt::LeftToRight ?
                      QLatin1String("edit-clear-locationbar-rtl") :
                      QLatin1String("edit-clear-locationbar-ltr"),
-                     QIcon::fromTheme(QLatin1String("edit-clear"), QIcon(QLatin1String(":/images/clear.png"))));
+                     QIcon::fromTheme(QLatin1String("edit-clear"), QIcon(QLatin1String(":/images/clear2.png"))));
 
     QIcon menuIcon(":/images/magnifier.png");
 
