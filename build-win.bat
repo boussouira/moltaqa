8bc53d37410b19cde4f1613d23aae6716617e802
Ahmed Saidi <justroftest@gmail.com>
2013-04-17 12:13:12 +0000

    Misc

--- build-win.bat
+++ build-win.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set QTDIR=C:/Qt/qt4-build
+set CLUCENEDIR=C:/clucene-vc
 set OUTDIR=moltaqa-lib-1.0b1
 
 echo Create directory %OUTDIR%
@@ -11,31 +13,31 @@ mkdir bin
 echo Copy files:
 
 echo -- Qt dlls
-cp C:/Qt/qt-vc-4.8.1/bin/phonon4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtCore4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtGui4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtNetwork4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtSql4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtWebKit4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtXmlPatterns4.dll bin
-cp C:/Qt/qt-vc-4.8.1/bin/QtXml4.dll bin
+cp %QTDIR%/bin/phonon4.dll bin
+cp %QTDIR%/bin/QtCore4.dll bin
+cp %QTDIR%/bin/QtGui4.dll bin
+cp %QTDIR%/bin/QtNetwork4.dll bin
+cp %QTDIR%/bin/QtSql4.dll bin
+cp %QTDIR%/bin/QtWebKit4.dll bin
+cp %QTDIR%/bin/QtXmlPatterns4.dll bin
+cp %QTDIR%/bin/QtXml4.dll bin
 
 echo -- Qt Drivers
 
 echo ---- SQL
 mkdir bin\sqldrivers
-cp C:/Qt/qt-vc-4.8.1/plugins/sqldrivers/qsqlite4.dll bin/sqldrivers
-cp C:/Qt/qt-vc-4.8.1/plugins/sqldrivers/qsqlodbc4.dll bin/sqldrivers
+cp %QTDIR%/plugins/sqldrivers/qsqlite4.dll bin/sqldrivers
+cp %QTDIR%/plugins/sqldrivers/qsqlodbc4.dll bin/sqldrivers
 
 echo ---- Images
 mkdir bin\imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qgif4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qico4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qjpeg4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qmng4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qsvg4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qtga4.dll bin/imageformats
-cp C:/Qt/qt-vc-4.8.1/plugins/imageformats/qtiff4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qgif4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qico4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qjpeg4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qmng4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qsvg4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qtga4.dll bin/imageformats
+cp %QTDIR%/plugins/imageformats/qtiff4.dll bin/imageformats
 
 echo -- MSVC dlls
 cp D:/Programming/albahhet-vc_release/msvcm90.dll bin
@@ -44,9 +46,10 @@ cp D:/Programming/albahhet-vc_release/msvcr90.dll bin
 cp D:/Programming/albahhet-vc_release/Microsoft.VC90.CRT.manifest bin
 
 echo -- CLucene dlls
-cp C:/clucene-vc/bin/Release/clucene-contribs-lib.dll bin
-cp C:/clucene-vc/bin/Release/clucene-core.dll bin
-cp C:/clucene-vc/bin/Release/clucene-shared.dll bin
+
+cp %CLUCENEDIR%/bin/Release/clucene-contribs-lib.dll bin
+cp %CLUCENEDIR%/bin/Release/clucene-core.dll bin
+cp %CLUCENEDIR%/bin/Release/clucene-shared.dll bin
 
 echo -- ZLib dll
 cp D:/Programming/albahhet-vc_release/zlib1.dll bin
